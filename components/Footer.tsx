6adc07d6b6a22d48018a2aa23f74aaa3666e4357
iJeannne <166218229+iJeannne@users.noreply.github.com>
2024-07-06 16:48:18 +0300

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <div id="footer"> 
+            <footer>Yes, this is footer</footer>
+        </div>
+    );
+};
+
+export default Footer;
